44c08a2699405ed63d7de45041f71d02b42ce09c
Omnironan <rshortall@omnifone.com>
2014-05-14 14:24:54 +0100

    Added package reference to TestComponent1
    Added package reference and method to exercise TestComponent1

--- TestComponent2/TestComponent2/Class1.cs
+++ TestComponent2/TestComponent2/Class1.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using TestComponent1;
 
 namespace TestComponent2
 {
     public class Class1
     {
+        public string GetName()
+        {
+            //Take advatange of the dependency on TestComponent1 nuget package
+            var shape = new MyShape("Test Name");
+            shape.ChangeName("New Name");
+
+            return shape.Name;
+        }
     }
 }
